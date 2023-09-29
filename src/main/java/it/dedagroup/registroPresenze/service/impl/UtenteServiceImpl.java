d91c87482aedfdfa49d503a92792f7a382406493
MRCMNL01L <MRCMNL01L@HP5FRV3.nco.inet>
2023-09-29 17:56:36 +0200

    creazione package: -DTO -facade -mapper -service(UtenteService) -service Impl (UtenteServiceImpl)

---
+++ src/main/java/it/dedagroup/registroPresenze/service/impl/UtenteServiceImpl.java
@@ -0,0 +1,10 @@
+package it.dedagroup.registroPresenze.service.impl;
+
+import it.dedagroup.registroPresenze.model.Utente;
+import it.dedagroup.registroPresenze.service.UtenteService;
+
+public class UtenteServiceImpl implements UtenteService{
+
+	
+
+}
