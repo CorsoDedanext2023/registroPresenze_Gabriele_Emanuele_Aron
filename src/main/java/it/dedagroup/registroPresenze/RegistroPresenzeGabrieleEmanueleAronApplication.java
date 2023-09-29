d261f3e92653552c71b34883043adbf1563fd136
DCTGRL01H <DCTGRL01H@1TLQTT3.nco.inet>
2023-09-29 16:53:30 +0200

    init del progetto con web starter

---
+++ src/main/java/it/dedagroup/registroPresenze/RegistroPresenzeGabrieleEmanueleAronApplication.java
@@ -0,0 +1,13 @@
+package it.dedagroup.registroPresenze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroPresenzeGabrieleEmanueleAronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroPresenzeGabrieleEmanueleAronApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/dedagroup/registroPresenze/RegistroPresenzeGabrieleEmanueleAronApplicationTests.java
@@ -0,0 +1,13 @@
+package it.dedagroup.registroPresenze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroPresenzeGabrieleEmanueleAronApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
